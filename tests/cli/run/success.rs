8050db029d0ff2ba1c6a2da264bedd7c3ed34530
Sean Kelleher <ezanmoto@gmail.com>
2022-04-19 20:35:37 +0000

    Add new subdirectory `mounts` test

--- tests/cli/run/success.rs
+++ tests/cli/run/success.rs
@@ -668,3 +668,46 @@ fn mount_proj_dir() {
     // (D)
     docker::assert_image_exists(&test.image_tagged_name);
 }
+
+#[test]
+// Given (1) the dock file defines an environment called `<env>`
+//     AND (2) `<env>` mounts `a/b` to `/host`
+//     AND (3) the subdirectory `a/b/c/d` contains `test.txt`
+// When `run <env> cat /host/c/d/test.txt` is run
+// Then (A) the command is successful
+//     AND (B) the command STDERR is empty
+//     AND (C) the command STDOUT contains the contents of `test.txt`
+//     AND (D) the target image exists
+fn mount_sub_dir() {
+    let test_name = "mount_sub_dir";
+    // (1)
+    let test = test_setup::assert_apply_with_dock_yaml(
+        // (2)
+        indoc!{"
+            mounts:
+              ./a/b: /host
+        "},
+        &Definition{
+            name: test_name,
+            fs: &hashmap!{
+                // (3)
+                "a/b/c/d/test.txt" => test_name,
+            },
+            dockerfile_steps: "",
+        },
+    );
+    docker::assert_remove_image(&test.image_tagged_name);
+    let args = &[test_name, "cat", "/host/c/d/test.txt"];
+
+    let cmd_result = run_test_cmd(test.dir, args);
+
+    cmd_result
+        // (A)
+        .code(0)
+        // (B)
+        .stderr("")
+        // (C)
+        .stdout(test_name.to_owned());
+    // (D)
+    docker::assert_image_exists(&test.image_tagged_name);
+}
